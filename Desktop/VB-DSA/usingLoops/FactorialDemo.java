8db8ac878eb967ce750e6a7404d4addf1e7e01db
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-07-19 11:11:21 +0530

    add

---
+++ Desktop/VB-DSA/usingLoops/FactorialDemo.java
@@ -0,0 +1,14 @@
+
+public class FactorialDemo {
+
+    public static void main(String[] args) {
+        int number = 5; // Example number to calculate factorial
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
